c25abb127865b0498770d757eef28234f2eb79d7
CinemaAddict <brokenswords21@gmail.com>
2023-01-22 00:34:59 +0300

    building homepage

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,5 +1,46 @@
+import styles from "./HomePage.module.scss";
+import appImageAuth from "../../assets/tabletHQ.png";
+import appImageInitial from "../../assets/initialHQ.png";
+import appImageChat from "../../assets/monitorHQ.png";
+
 const HomePage = () => {
-  return <>Welcome to Homepage!!!</>;
+  return (
+    <section className={styles.homepage}>
+      <div className={styles.container}>
+        <div className={styles.images}>
+          <div className={`${styles.tablet} ${styles.left}`}>
+            <div className={styles.content}>
+              <img src={appImageAuth} alt="app-promo" />
+            </div>
+          </div>
+          <div className={`${styles.stand} ${styles.middle}`}>
+            <div className={styles.monitor}>
+              <img src={appImageChat} alt="app-promo" />
+            </div>
+          </div>
+          <div className={`${styles.smartphone} ${styles.right}`}>
+            <div className={styles.content}>
+              <img src={appImageInitial} alt="app-promo" />
+            </div>
+          </div>
+        </div>
+        <ul className={styles.description}>
+          <li className={styles.items}>
+            <div>Title</div>
+            <div>description description description</div>
+          </li>
+          <li>
+            <div>Title</div>
+            <div>description description description</div>
+          </li>
+          <li>
+            <div>Title</div>
+            <div>description description description</div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default HomePage;
