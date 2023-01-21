930f160d8d0c8e6b4e98c92c8878cf6055a6cbc2
CinemaAddict <brokenswords21@gmail.com>
2023-01-22 02:43:26 +0300

    hompage section with screens

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -7,38 +7,40 @@ const HomePage = () => {
   return (
     <section className={styles.homepage}>
       <div className={styles.container}>
-        <div className={styles.images}>
-          <div className={`${styles.tablet} ${styles.left}`}>
-            <div className={styles.content}>
-              <img src={appImageAuth} alt="app-promo" />
+        <div className={styles["css-figures"]}>
+          <div className={styles.images}>
+            <div className={`${styles.tablet} ${styles.left}`}>
+              <div className={styles.content}>
+                <img src={appImageAuth} alt="app-promo" />
+              </div>
             </div>
-          </div>
-          <div className={`${styles.stand} ${styles.middle}`}>
-            <div className={styles.monitor}>
-              <img src={appImageChat} alt="app-promo" />
+            <div className={`${styles.stand} ${styles.middle}`}>
+              <div className={styles.monitor}>
+                <img src={appImageChat} alt="app-promo" />
+              </div>
             </div>
-          </div>
-          <div className={`${styles.smartphone} ${styles.right}`}>
-            <div className={styles.content}>
-              <img src={appImageInitial} alt="app-promo" />
+            <div className={`${styles.smartphone} ${styles.right}`}>
+              <div className={styles.content}>
+                <img src={appImageInitial} alt="app-promo" />
+              </div>
             </div>
           </div>
         </div>
-        <ul className={styles.description}>
-          <li className={styles.items}>
-            <div>Title</div>
-            <div>description description description</div>
-          </li>
-          <li>
-            <div>Title</div>
-            <div>description description description</div>
-          </li>
-          <li>
-            <div>Title</div>
-            <div>description description description</div>
-          </li>
-        </ul>
       </div>
+      <ul className={styles.description}>
+        <li className={styles.items}>
+          <div>Title</div>
+          <div>description description description</div>
+        </li>
+        <li>
+          <div>Title</div>
+          <div>description description description</div>
+        </li>
+        <li>
+          <div>Title</div>
+          <div>description description description</div>
+        </li>
+      </ul>
     </section>
   );
 };
