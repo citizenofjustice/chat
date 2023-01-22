6ef761a30de3ea32adc85858cc7139b6bf2603fc
CinemaAddict <brokenswords21@gmail.com>
2023-01-22 03:49:29 +0300

    styling frontpage

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -6,6 +6,11 @@ import appImageChat from "../../assets/monitorHQ.png";
 const HomePage = () => {
   return (
     <section className={styles.homepage}>
+      <h1 className={styles.title}>SIMPLE Chat</h1>
+      <p className={styles.tech}>
+        WEB-приложение разарботано на ReactJS, с использованием технологий
+        Redux/Redux-Toolkit, Redux-persist, React-Router, Firebase, SASS
+      </p>
       <div className={styles.container}>
         <div className={styles["css-figures"]}>
           <div className={styles.images}>
@@ -27,20 +32,50 @@ const HomePage = () => {
           </div>
         </div>
       </div>
-      <ul className={styles.description}>
-        <li className={styles.items}>
-          <div>Title</div>
-          <div>description description description</div>
-        </li>
-        <li>
-          <div>Title</div>
-          <div>description description description</div>
-        </li>
-        <li>
-          <div>Title</div>
-          <div>description description description</div>
-        </li>
-      </ul>
+      <div className={styles.desk} />
+      {/* <div>
+        <ul className={styles.description}>
+          <li className={styles.items}>
+            <div>Функционал</div>
+            <ul>
+              <li>Регистрация/Авторизация пользователя.</li>
+              <li>
+                Загрузка изображения на сервер, для отображения иконки профиля.
+              </li>
+              <li>
+                Взаимодействие с базой данных, для отправки и получения
+                сообщений. Обновление сообщений в реальном времени.
+              </li>
+              <li>
+                Смена электронной почты, пароля, никнейма, изображения профиля.
+                С поледующим отражением изменений в чате.
+              </li>
+              <li>
+                Хранение токена для сохранения сессии авторизированного
+                пользователя в течении часа. По его истечению принудительное
+                удаление токена.
+              </li>
+              <li>
+                Наличие адаптивной верстки для удобного отображения на
+                устройствах с разной шириной экрана.
+              </li>
+            </ul>
+          </li>
+          <li>
+            <div>Для начала работы:</div>
+            <div>
+              Чтобы воспользоваться приложением, требуется регистрация с помощью
+              электронной почты и пароля. Затем нужно инициализировать учетную
+              запись, а именно выбрать никнейм, который бутет отображаться у вас
+              в чате.
+            </div>
+          </li>
+          <li>
+            <div>Title</div>
+            <div>description description description</div>
+          </li>
+        </ul>
+      </div> */}
     </section>
   );
 };
